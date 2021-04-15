9b08780baf0820795fb181a12d41d3fcc746eeb2
KingMarine-GH <kingmarineofov@gmail.com>
2021-04-16 02:18:03 +0800

    added monitoring

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 
 const Discord = require("discord.js"),
       client = new Discord.Client(),
-      axios = require("axios").default.create();
+      axios = require("axios").default;
 
+// Monitor
+// REMINDME to uncomment this area
+/*
+axios.post(process.env.MONITOR);
+const monitor = setInterval(() => {
+    axios.post(process.env.MONITOR);
+}, 300000);
+*/
 client.once("ready", () => {
     console.log("Ready!");
 });
